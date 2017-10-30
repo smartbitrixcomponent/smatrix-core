ffcdf6dbfb98db8aa3b2b69b4dfc91de69885ca6
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2017-10-30 18:07:48 +0200

    Добавляет возможность подключать внешние CSS и  JS внутри шаблонов

--- local/templates/site/components/bitrix/button/default/template.php
+++ local/templates/site/components/bitrix/button/default/template.php
@@ -1,1 +1,2 @@
-<button class="general-button js-general-button">im button</button>
+<button class="general-button js-general-button">im button22</button>
+<?$this->addExternalCss("http://www.csszengarden.com/214/214.css");?>
